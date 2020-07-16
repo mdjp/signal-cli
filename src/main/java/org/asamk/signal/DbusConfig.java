6a983c96157de9ded73304a89710ef3b219a05c4
Matthew Paradis <matthew.paradis@bbc.co.uk>
2020-07-16 11:51:13 +0100

    dev

--- src/main/java/org/asamk/signal/DbusConfig.java
+++ src/main/java/org/asamk/signal/DbusConfig.java
@@ -2,6 +2,6 @@ package org.asamk.signal;
 
 public class DbusConfig {
     
-    public static final String SIGNAL_BUSNAME = "org.asamk.Signal." + "447860047010";
+    public static final String SIGNAL_BUSNAME = "org.asamk." + "447860047010";
     public static final String SIGNAL_OBJECTPATH = "/org/asamk/Signal";
 }
